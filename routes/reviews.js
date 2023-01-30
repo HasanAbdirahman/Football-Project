c5af1ecfde159301ec11e93d2c8ba05db6f502fd
Hasan Abdirahman <hasanabdirahman.ao@gmail.com>
2023-01-30 17:07:31 -0500

    finished

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 let reviewsCtrl = require("../controller/reviews");
 
-router.post("/reviews/:id/comments", reviewsCtrl.create);
+router.post("/:id/comments", reviewsCtrl.create);
 router.delete("/players/:pid/reviews/:rid", reviewsCtrl.delete);
 router.get("/players/:pid/reviews/:rid", reviewsCtrl.edit);
 router.put("/players/:pid/reviews/:rid", reviewsCtrl.update);
